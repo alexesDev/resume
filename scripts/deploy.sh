6dcad6df938e99d2d6c7098f78de44e7b77250e3
Alexey Yurchenko <alexes.dev@gmail.com>
2024-10-17 13:04:20 +0000

    feat(scripts/deploy.sh): make a commit only if has changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,10 @@
 
 set -e
 
-commit_message=$1
-
-if [ -z "$commit_message" ]; then
-  echo "Usage: $0 <commit message>"
-  exit 1
-fi
-
 echo "Preparing to push..."
 
 ./scripts/prepare.sh
 
-git add .
-git commit -m "$commit_message" || true
+git add en.html ru.pdf en.pdf
+git commit -m "prepare generated content" || true # or skip
 git push
